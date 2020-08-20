e7704c12be027e3c4aea037a74bb0afd45ceae34
Lvaughn mason <lvaughnm@hotmail.com>
2020-08-20 00:31:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,16 +8,3 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-
-
- board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-
-      expect(rows[0]).to eq(" X | O | X ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | X | X ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | X | O ")
- 
