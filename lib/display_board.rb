a408f2c5e1ed32598f3268b7391f965f13772bce
Lvaughn mason <lvaughnm@hotmail.com>
2020-08-20 00:30:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["  ", "  ", "  ", " ", "  ", " ", " ", " ", " "]
+
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
+ board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | X | O ")
+ 
